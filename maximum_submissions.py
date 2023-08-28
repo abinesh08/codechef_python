c2c42cd6a996e9e52a0f1c22873e66463e1f2f35
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2023-08-29 02:25:11 +0530

    Create maximum_submissions.py
    Code-10

---
+++ maximum_submissions.py
@@ -0,0 +1,8 @@
+""" A participant can make 1 submission every 30 seconds. If a contest lasts for 
+X minutes, what is the maximum number of submissions that the participant can make during it?
+It is also given that the participant cannot make any submission in the last 
+5 seconds of the contest. """
+
+for _ in range(int(input())):
+    x=int(input())
+    print(x*2)
