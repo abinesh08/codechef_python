69aebde7a92a073a886f417c0d4c64735e1f57bd
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2023-08-29 00:29:00 +0530

    Create Candy store.py

---
+++ Candy store.py
@@ -0,0 +1,15 @@
+""" Chef has started working at the candy store. The store has 100 chocolates in total.
+
+Chef’s daily goal is to sell X chocolates. For each chocolate sold, he will get 
+1 rupee. However, if Chef exceeds his daily goal, he gets 
+2 rupees per chocolate for each extra chocolate.
+If Chef sells 
+Y chocolates in a day, find the total amount he made.  """
+
+for _ in range(int(input())):
+    x,y=map(int,input().split())
+    if x>=y:
+        print(y)
+    elif x<y:
+        z=y-x
+        print(x + (z *2))
