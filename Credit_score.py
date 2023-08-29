e80787bb112e9cc6b6939c5d3a9862035824d512
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2023-08-30 01:15:21 +0530

    Code 21

---
+++ Credit_score.py
@@ -0,0 +1,13 @@
+#code21
+""" To access CRED programs, one needs to have a credit score of 
+750
+750 or more.
+Given that the present credit score is 
+�
+X, determine if one can access CRED programs or not. """
+
+
+X= int(input())
+if X>=750:
+    print("Yes")
+else:
