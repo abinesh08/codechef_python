40fe400bc92de35376f9e713be9433242be5b8e9
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2023-09-01 18:53:32 +0530

    code 51

---
+++ 51 office.py
@@ -0,0 +1,8 @@
+""" Recently Chef joined a new company. In this company, the employees have to work for 
+X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for Y hours (Y<X) on Friday. Saturdays and Sundays are holidays.
+Determine the total number of working hours in one week. """
+
+for _ in range(int(input())):
+    x,y=map(int,input().split())
+    w= x*4
+    print(w+y)
