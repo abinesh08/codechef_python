320f13fed32440e67dba6f9b8ec8f7e83867abda
Abinesh R <74003839+abinesh08@users.noreply.github.com>
2023-09-05 21:48:41 +0530

    Code 63

---
+++ 63 CAR_OR_BIKE.py
@@ -0,0 +1,15 @@
+""" Chef wants to reach home as soon as possible. He has two options:
+
+Travel with his BIKE which takes 
+X minutes.
+Travel with his CAR which takes Y minutes.
+Which of the two options is faster or do they both take same time? """
+
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    if a > b :
+        print("BIKE")
+    elif a<b:
+        print("CAR")
+    else:
+        print("SAME")
